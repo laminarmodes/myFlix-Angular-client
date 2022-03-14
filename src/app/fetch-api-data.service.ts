e6ec0a01ff2f3863272409ec4109f1f8cd48274b
laminarmodes <anya.traille@gmail.com>
2022-03-14 13:03:13 +0800

    First Commit

---
+++ src/app/fetch-api-data.service.ts
@@ -0,0 +1,63 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError, catchError } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+// Declaring the api URL that will provide data for the client app
+const apiUrl = 'https://myflixappcf.herokuapp.com/'
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class UserRegistrationService {
+  // Inject the HttpClient module to the constructor params
+  // This will provide HttpClient to the entire class, making it available via this.http
+  constructor(private http: HttpClient) {
+
+  }
+  // Making the api call for the user registration endpoint
+  public userRegistration(userDetails: any): Observable<any> {
+    console.log(userDetails);
+    // Posts to the endpoint and returns the API response
+    return this.http.post(apiUrl + 'users', userDetails).pipe(catchError(this.handleError));
+  }
+
+  private handleError(error: HttpErrorResponse): any {
+    if (error.error instanceof ErrorEvent) {
+      console.error('Some error occurred', error.error.message);
+    } else {
+      console.error(
+        `Error Status code ${error.status},` +
+        `Error body is: ${error.error}`
+      );
+    }
+    return throwError(
+      'Something bad happened; please try again later.'
+    );
+  }
+
+  getAllMovies(): Observable<any> {
+    const token = localStorage.getItem('token');
+    return this.http.get(apiUrl + 'movies', {
+      headers: new HttpHeaders(
+        {
+          Authorization: 'Bearer ' + token,
+        }
+      )
+    }).pipe(
+      map(this.extractResponseData),
+      catchError(this.handleError)
+    );
+  }
+  // Non-typed response extraction
+  private extractResponseData(data: any | Object): any {
+    const body = data;
+    return body || {};
+  }
+
+
+
+}
+
+
